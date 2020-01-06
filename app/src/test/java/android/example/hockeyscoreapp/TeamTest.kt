2e8663024c284300e0d69acea31d950545ca246b
Spartanrein <reiner.deguzman@zuhlke.com>
2020-01-06 14:17:06 +0800

    refactor()

--- app/src/test/java/android/example/hockeyscoreapp/TeamTest.kt
+++ app/src/test/java/android/example/hockeyscoreapp/TeamTest.kt
@@ -2,22 +2,26 @@ package android.example.hockeyscoreapp
 
 import org.junit.Assert.assertEquals
 import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.Test
 
 class TeamTest {
 
     private val team: Team = Team()
 
+    @Before
+    fun beforeEach() {
+
+    }
+
     @Test
     fun `should increment goals, missed, and blocked to 1, and shots to 3`() {
         team.incrementStat(Team.Stat.GOALS)
         team.incrementStat(Team.Stat.MISSED)
         team.incrementStat(Team.Stat.BLOCKED)
-        assertEquals(1,team.goals)
-        assertEquals(1,team.missed)
-        assertEquals(1,team.blocked)
-        assertEquals(3,team.shots)
+        assertEquals(1, team.goals)
+        assertEquals(1, team.missed)
+        assertEquals(1, team.blocked)
+        assertEquals(3, team.shots)
 
     }
 }
